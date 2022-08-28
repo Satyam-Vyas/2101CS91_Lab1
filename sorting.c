39b7ac9e9d8cf391f3d2681c1b57d5756303499a
satyamvyas <vyas.satyam10@gmail.com>
2022-08-28 15:53:38 +0530

    completed selection sort

--- sorting.c
+++ sorting.c
@@ -34,6 +34,29 @@ void insertionSort(int a[], int n)
     }
 }
 
+void selectionSort(int a[], int n)
+{
+    int i, j, min_idx;
+
+    // moving boundary of unsorted subarray
+    for (i = 0; i < n - 1; i++)
+    {
+        // Finding minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min_idx])
+                min_idx = j;
+        }
+
+        // Swapping found and first element
+        if (min_idx != i)
+        {
+            swap(&a[min_idx], &a[i]);
+        }
+    }
+}
+
 int main()
 {
     int n, m, a[1000];
