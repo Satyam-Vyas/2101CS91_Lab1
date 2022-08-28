7b028c0e6cce749a98eca4992a7e713bc4c86431
satyamvyas <vyas.satyam10@gmail.com>
2022-08-28 15:24:35 +0530

    completed main function

---
+++ sorting.c
@@ -0,0 +1,46 @@
+// C program to sort an array using different sorting methods
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, m, a[1000];
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter array elements\n");
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("methods to sort your array\n");
+    printf("1- Insertion Sort,2- Selection Sort,3- Bubble Sort,4- Merge Sort,5- Quick Sort \n");
+    printf("Enter preferred Sorting Method: ");
+    switch (m)
+    {
+    case 1:
+        insertionSort(a, n);
+        print(a, n);
+        break;
+    case 2:
+        selectionSort(a, n);
+        print(a, n);
+        break;
+    case 3:
+        bubbleSort(a, n);
+        print(a, n);
+        break;
+    case 4:
+        mergeSort(a, 0, n - 1);
+        print(a, n);
+        break;
+    case 5:
+        quicksort(a, 0, n - 1);
+        print(a, n);
+        break;
+    default:
+        printf("Please enter valid option of sorting method");
+        break;
+    }
+    scanf("%d", &m);
+    return 0;
+}
