1c1140f999fb34624cbacab2a3314f8157fd23eb
satyamvyas <vyas.satyam10@gmail.com>
2022-08-28 15:08:04 +0530

    completed multiplication and division program

--- calculator.c
+++ calculator.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main()
 {
     char c;
     double a1, a2;
-    while (1) {
+    while (1)
+    {
         printf("Enter an operator (+, -, *, /), if want to exit enter '!': ");
         scanf(" %c", &c);
-        if (c == '!') //exit condition
+        if (c == '!') // exit condition
             exit(0);
         printf("Enter two first and second operand: ");
-        scanf("%lf %lf",&a1,&a2);
-        switch (c) {
-        //Addition
+        scanf("%lf %lf", &a1, &a2);
+        switch (c)
+        {
+        // Addition
         case '+':
             printf("%.2lf + %.2lf = %.2lf\n", a1, a2, a1 + a2);
             break;
-        //Subtraction
+        // Subtraction
         case '-':
             printf("%.2lf - %.2lf = %.2lf\n", a1, a2, a1 - a2);
             break;
+        // Multiplication
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", a1, a2, a1 * a2);
+            break;
+        // Division
+        case '/':
+            printf("%.2lf / %.2lf = %.2lf\n", a1, a2, a1 / a2);
+            break;
+        default:
+            printf("Invalid operator chosen!\n");
         }
-         }
+    }
     return 0;
 }
