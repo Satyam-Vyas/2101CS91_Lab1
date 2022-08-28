991243cc4202c62cde5e157370a417cf13dcee50
satyamvyas <vyas.satyam10@gmail.com>
2022-08-28 16:01:11 +0530

    completed bubble sort

--- sorting.c
+++ sorting.c
@@ -57,6 +57,17 @@ void selectionSort(int a[], int n)
     }
 }
 
+void bubbleSort(int arr[], int n) 
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]); //swapping
+        }
+    }
+}
+
 int main()
 {
     int n, m, a[1000];
